8e8caecbe67235ef3117fb7ebe99d677e5f28d5d
Steven Masley <stevenmasley@gmail.com>
2017-01-26 19:04:20 -0600

    Added some marshaling utils, and uil

---
+++ util/util.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"os"
+	"os/user"
+)
+
+func GetHomeDir() string {
+	// Get the OS specific home directory via the Go standard lib.
+	var homeDir string
+	usr, err := user.Current()
+	if err == nil {
+		homeDir = usr.HomeDir
+	}
+
+	// Fall back to standard HOME environment variable that works
+	// for most POSIX OSes if the directory from the Go standard
+	// lib failed.
+	if err != nil || homeDir == "" {
+		homeDir = os.Getenv("HOME")
+	}
+	return homeDir
+}
---
+++ util/util_test.go
@@ -0,0 +1,12 @@
+package util_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/DistributedSolutions/DIMWIT/util"
+)
+
+func TestHomeDor(t *testing.T) {
+	GetHomeDir()
+}
