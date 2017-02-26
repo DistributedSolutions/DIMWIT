8f1c79cc2491e3ac4efe1dd7b68568de1942f7fb
Steven Masley <stevenmasley@gmail.com>
2017-02-25 21:14:46 -0600

    Added sql writer

---
+++ constructor/sqlWriter.go
@@ -0,0 +1,68 @@
+package constructor
+
+import (
+	"time"
+
+	"github.com/DistributedSolutions/DIMWIT/common"
+)
+
+const (
+	LOOP_DELAY time.Duration = time.Duration(1 * time.Second)
+)
+
+type SqlWriter struct {
+	// Incoming channels to write to sql db
+	channelQueue chan common.Channel
+
+	// Stop goroutine
+	quit chan int
+}
+
+func NewSqlWriter() *SqlWriter {
+	sw := new(SqlWriter)
+	sw.quit = make(chan int, 5)
+	sw.channelQueue = make(chan common.Channel, 1000)
+
+	return sw
+}
+
+// Close sqlwriter
+func (sw *SqlWriter) Close() {
+	sw.quit <- 0
+}
+
+func (sw *SqlWriter) DrainChannelQueue() {
+	for {
+		// Closeing sqlwrite
+		select {
+		case <-sw.quit:
+			// Add your close code here
+			return
+		default:
+		}
+
+		// Take incoming channels
+		select {
+		case channel := <-sw.channelQueue:
+			channelList := make([]common.Channel, 0)
+			channelList = append(channelList, channel)
+			// Do stuff
+			length := len(sw.channelQueue)
+			for i := 0; i < length; i++ {
+				select {
+				case newChan := <-sw.channelQueue:
+					channelList = append(channelList, newChan)
+				default:
+				}
+			}
+
+			// ChannelList, play with it
+			// Do your write
+		default:
+			// Nothing really
+		}
+
+		// Don't starve other routines
+		time.Sleep(LOOP_DELAY)
+	}
+}
