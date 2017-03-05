9a1ea0326b0c3e61ec33160fa632d3a2990c4892
Steven Masley <stevenmasley@gmail.com>
2017-03-05 12:03:26 -0600

    Fixed tests

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -5,69 +5,20 @@ import (
 	"testing"
 	"time"
 
-	"github.com/DistributedSolutions/DIMWIT/channelTool"
-	"github.com/DistributedSolutions/DIMWIT/channelTool/creation"
-	"github.com/DistributedSolutions/DIMWIT/common"
-	"github.com/DistributedSolutions/DIMWIT/common/primitives"
 	"github.com/DistributedSolutions/DIMWIT/constructor"
 	"github.com/DistributedSolutions/DIMWIT/database"
-	"github.com/DistributedSolutions/DIMWIT/factom-lite"
 	. "github.com/DistributedSolutions/DIMWIT/provider"
+	"github.com/DistributedSolutions/DIMWIT/testhelper"
 )
 
 var _ = fmt.Sprintf("")
 var _ = time.Second
 
 func TestProvider(t *testing.T) {
-	fake := lite.NewFakeDumbLite()
-	m := creation.NewMasterChain()
-	ec := lite.GetECAddress()
-	fake.SubmitChain(*m.Chain, *ec)
-	//fake := lite.NewDumbLite()
-	chanList := make([]common.Channel, 0)
-
-	for i := 0; i < 5; i++ {
-		ch := common.RandomNewChannel()
-		auth, err := channelTool.NewAuthChannel(ch, ec)
-		if err != nil {
-			t.Error(err)
-		}
-
-		chanList = append(chanList, auth.Channel)
-
-		chains, err := auth.ReturnFactomChains()
-		if err != nil {
-			t.Error(err)
-		}
-
-		entries, err := auth.ReturnFactomEntries()
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, c := range chains {
-			_, _, err := fake.SubmitChain(*c, *ec)
-			if err != nil {
-				t.Error(err)
-			}
-		}
-
-		eHashes := make([]string, 0)
-		for _, e := range entries {
-			_, ehash, err := fake.SubmitEntry(*e, *ec)
-			if err != nil {
-				t.Error(err)
-			}
-			eHashes = append(eHashes, ehash)
-		}
-
-		for _, h := range eHashes {
-			hash, _ := primitives.HexToHash(h)
-			_, err := fake.GetEntry(*hash)
-			if err != nil {
-				t.Error(err)
-			}
-		}
+	fake, chanList, err := testhelper.PopulateFakeClient(true, 5)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
 	}
 
 	db := database.NewMapDB()
@@ -79,8 +30,10 @@ func TestProvider(t *testing.T) {
 
 	con.SetReader(fake)
 	go con.StartConstructor()
-
 	max, _ := con.Reader.GetReadyHeight()
+	if max == 0 {
+		max = 1
+	}
 	for con.CompletedHeight < max-1 {
 		time.Sleep(200 * time.Millisecond)
 		// fmt.Println(con.CompletedHeight, max)
@@ -90,11 +43,12 @@ func TestProvider(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	// defer prov.Close()
 
 	for _, c := range chanList {
 		nc, err := prov.GetChannel(c.RootChainID.String())
 		if err != nil {
-			t.Error(err)
+			t.Error("Not found,", err.Error())
 			t.FailNow()
 		}
 		if !nc.IsSameAs(&c) {
