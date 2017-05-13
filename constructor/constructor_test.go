f314a62c93cf93bdca2a9cf04ef16870e98393f0
Steven Masley <stevenmasley@gmail.com>
2017-05-13 11:38:36 -0500

    import fix

--- constructor/constructor_test.go
+++ constructor/constructor_test.go
@@ -11,7 +11,7 @@ import (
 	. "github.com/DistributedSolutions/DIMWIT/constructor"
 	"github.com/DistributedSolutions/DIMWIT/database"
 	//"github.com/DistributedSolutions/DIMWIT/factom-lite"
-	"github.com/DistributedSolutions/DIMWIT/testHelper"
+	"github.com/DistributedSolutions/DIMWIT/testhelper"
 )
 
 var _ = fmt.Sprintf("")
